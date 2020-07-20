7f02c3cac0038fb3f01c0f19239e7e23f0a78d79
ronka2 <50966133+ronka2@users.noreply.github.com>
2020-07-20 15:38:32 +0200

    encryption base file
    encrypts/decrypts text from file

---
+++ PWMCrypto.py
@@ -0,0 +1,47 @@
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+
+output_file = "encryptedFile.txt"
+input_file = "test.txt"
+salt = get_random_bytes(16)
+
+file_in = open(input_file,"r")
+file_in = file_in.read().encode()
+
+def keyDerivingFunction(password,salt):
+    return PBKDF2(password,salt,32)
+ 
+def encryption(key,data,output_file):
+    cipher = AES.new(key,AES.MODE_EAX)
+    cipherText,tag = cipher.encrypt_and_digest(data)
+    print(cipherText)
+    file_out = open(output_file,"wb")
+    file_out.write(cipher.nonce)
+    file_out.write(tag)
+    file_out.write(cipherText)
+    file_out.close()
+
+def decryption(key,encFile):
+    encFile = open(encFile,"rb")
+    nonce = encFile.read(16)
+    tag = encFile.read(16)
+    encData = encFile.read()
+    cipher = AES.new(key, AES.MODE_EAX,nonce=nonce)
+    plaintext = cipher.decrypt(encData)
+    try:
+        cipher.verify(tag)
+        print("good to go boiz: ", plaintext.decode())
+    except ValueError:
+        print("incorrect key or corrupted message")
+
+
+
+key = keyDerivingFunction("hello",salt)
+encryption(key,file_in,output_file)
+decryption(key,output_file)
+
+
+
+
