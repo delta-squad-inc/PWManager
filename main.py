f6cc048aa7da300ece34688486cca81d6ac77d9c
freudwasright <57996039+freudwasright@users.noreply.github.com>
2020-12-19 16:29:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import redis
 
 def main():
     #asd = {"key": [124514155144844, 151518541851]}
-    pwmanager = redismodel.PasswordManager(6379, 'localhost', 0, _password="pwd1111")
+    pwmanager = redismodel.PasswordManager(6379, 'localhost', 0, _password="")
     #pwmanager.setValue("facebook", "pwd123456789")
     #response = pwmanager.getValue("facebook")
     #pwmanager.setHash("gmail", ('111', '112', '15'))
